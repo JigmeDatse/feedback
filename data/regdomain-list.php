58f68e19f9819e08e88a98e4095ee09657f0a5fd
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-09-25 20:00:15 +0000

    Starting data segragation
    This is the start to keep the data files out of the way of the other
    files.  This is not doing anything yet (though will be looking at
    restricting access on the server).

---
+++ data/regdomain-list.php
@@ -0,0 +1,14 @@
+<?php
+
+$regDomainNames=array(
+                        "regdomain1" => "inputfeedback.com",
+                        "regdomain2" => "www.inputfeedback.com",
+                        "regdomain3" => "feedbackinput.com",
+                        "regdomain4" => "www.feedbackinput.com",
+                        "regdomain5" => "reviewinput.com",
+                        "regdomain6" => "www.reviewinput.com",
+                        "regdomain7" => "notreallyadomain.com"
+);
+
+
+?>
