1160f6cb7883de770b9238ab9e727b0af399afeb
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-09-25 19:01:27 +0000

    Start of form input
    Just starting to get some idea of how to create and display the form.  I
    think a lot of this will need to be refactored as I start playing better
    with how I will represent the data that I get from this form.

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php 
 
-$sitename=$_SERVER['SERVER_NAME'];
+
+$sitename=$_SERVER['HTTP_HOST'];
+
+include "regdomains-list.php";
+
+/*
+$regDomainNames=array(
+			"regdomain1" => "inputfeedback.com",
+			"regdomain2" => "www.inputfeedback.com",
+			"regdomain3" => "feedbackinput.com",
+			"regdomain4" => "www.feedbackinput.com",
+			"regdomain5" => "reviewinput.com",
+			"regdomain6" => "www.reviewinput.com",
+			"regdomain7" => "notreallyadomain.com"
+);
+*/
 
 $title=$sitename." — Main Page";
 $h1="Welcome to ".$sitename.".  Please look about";
@@ -19,6 +34,16 @@ domains.  This means that it could be different sites.  Currently we have:</p>
     <li>reviewinput.com</li>
     <li>www.reviewinput.com</li>
 </ul>";
+
+echo '<form action="record.php" method="post">';
+
+foreach ($regDomainNames as $regNameID => $regDomainName) {
+	echo '<input type="checkbox" name="'.$regNameID.'" id="'.$regNameID.'" value="'.$regDomainName.'"><label for="'.$regNameID.'">'.$regDomainName.'</label><br />';
+}
+
+echo '	<label for="other">Other:<input type="text" id="other" name="other">
+	<input type="submit" value="submit" name="submit">
+</form>';
 
 include "footer.php";
 
